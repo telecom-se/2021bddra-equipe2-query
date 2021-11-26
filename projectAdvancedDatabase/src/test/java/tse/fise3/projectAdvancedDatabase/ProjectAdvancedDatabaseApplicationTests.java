f6539b08a9faa87f58d5d17c999f3af513088484
Burbanit0 <72401931+Burbanit0@users.noreply.github.com>
2021-11-26 11:16:40 +0100

    first commit

---
+++ projectAdvancedDatabase/src/test/java/tse/fise3/projectAdvancedDatabase/ProjectAdvancedDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package tse.fise3.projectAdvancedDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAdvancedDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
